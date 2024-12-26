0fbd8b8d14665822f960d7b8c1d1e707d352421f
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2024-12-27 00:47:31 +0530

    Shorten the home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -92,41 +92,18 @@
      I have been a Google Summer of Code contributor at <a class="link hover-effect" href="https://eunomia.dev/">eunomia-bpf</a>.
     </p>
     <p>
-      I have done B.Tech from <a class="link hover-effect" href="https://www.svnit.ac.in/">SVNIT</a>.
-      I got interested in computers when I was goofing around with my cousins and they were talking about Bill Gates, Steve Jobs.
-      I was learning about different things related to computers. Then in school I had a computer course where I learnt about <a class="link hover-effect" href="https://qb64.com/">QBasic</a>.
+      I graduated with a B.Tech from <a class="link hover-effect" href="https://www.svnit.ac.in/">SVNIT</a>. My journey started with web development at <a class="link hover-effect" href="https://www.freecodecamp.org/">FreeCodeCamp</a>, 
+      leading to machine learning projects in college. A pivotal moment was discovering Operating Systems: Three Easy Pieces (<a class="link hover-effect" href="https://pages.cs.wisc.edu/~remzi/OSTEP/">OSTEP</a>), 
+      which sparked my deep interest in systems programming.
     </p>
 
     <p>
-      I along with my friends started looking out on resources to create a snake game in QBasic. Then in high school, I started learning about 
-      html, css and javascript. I came across <a class="link hover-effect" href="https://www.freecodecamp.org/">Freecodecamp</a> and their extensive curriculum on web development.
+      An inspiring <a class="link hover-effect" href="https://speakerdeck.com/madhavjivrajani/can-we-use-ebpf-to-debug-performance-of-the-go-scheduler">eBPF talk</a> and this fascinating <a class="link hover-effect" href="https://youtu.be/Wb_vD3XZYOA?si=taGvQCXNvSsoVGk0">documentary</a> 
+      led me to contribute to <a class="link hover-effect" href="https://eunomia.dev/">eunomia-bpf</a> for GSoC'24, where I work on their innovative userspace eBPF runtime.
     </p>
 
     <p>
-      I started surfing internet for how can I create stuff. I recall I had also started learning python from the
-      official documentation. I have not been able to complete it and it makes no sense to complete it as it is a documentation not a learning journal.
-    </p>
-
-    <p> 
-      I came across Google Code-in and I learnt about open source contribution. That began my journey of open source contribution and curiosity to explore 
-      more on the emerging technologies of the world. I learnt <a class="link hover-effect" href="https://www.amazon.in/Computer-Science-Class-12-Examination/dp/8177000241">C++</a> in my 11-12th grade and then in college, I started learning about Machine learning.
-      I created couple of projects in ML with a technical club of my college.
-    </p>
-
-    <p>
-      After graduating from college, I started exploring more on systems and how 
-      Operating systems, languages and compilers work. I came across Operating Systems: Three Easy Pieces (It is free to read <a class="link hover-effect" href="https://pages.cs.wisc.edu/~remzi/OSTEP/">here</a>) book and read it cover to cover. I loved every bit of it.
-    </p>
-
-    <p>
-      I started attending meetups in my city, heard about eBPF and that <a class="link hover-effect" href="https://speakerdeck.com/madhavjivrajani/can-we-use-ebpf-to-debug-performance-of-the-go-scheduler">eBPF talk</a> by Madhav Jivrajani and Raghav remained ingrained in my mind. 
-      Then there came a documentary on youtube about <a class="link hover-effect" href="https://youtu.be/Wb_vD3XZYOA?si=taGvQCXNvSsoVGk0">eBPF</a> I watched it and I was amazed to learn of its beginnings.
-    </p>
-
-    <p>
-      So when GSoC'24 came, I looked for projects on eBPF and I found <a class="link hover-effect" href="https://eunomia.dev/">eunomia-bpf</a>. I was amazed to see that they had created a 
-      userspace runtime for eBPF which is by nature a kernel space technology. I got into the project and I am still contributing to it. 
-      These days, I am learning more on distributed systems, doing some projects on codecrafters.io and looking for an opportunity to work in a more system oriented role.
+      Currently, I'm deepening my knowledge of distributed systems, working on system design challenges at codecrafters.io, and seeking opportunities in systems engineering roles.
     </p>
 
     <p class="interests">Other interests: math, reading, football, cricket and basketball.</p>
