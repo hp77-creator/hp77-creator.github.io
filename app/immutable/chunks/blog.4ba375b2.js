28bd416ed516411685325a810edeb9015f682408
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2025-09-01 22:43:30 +0530

    Updates

--- app/immutable/chunks/blog.4ba375b2.js
+++ app/immutable/chunks/blog.4ba375b2.js
@@ -58,6 +58,45 @@ When you build some C++ program, you have to first check the kind of build syste
 ## References
 
 - [Git remote branches](https://git-scm.com/book/en/v2/Git-Branching-Remote-Branches)
+`,d=`---
+title: Adding a FRM parser to MariaDB 
+date: 2025-06-23
+description: A GSoC project 
+visible: false
+tags: database, mariaDB, C++
+---
+
+In one of my previous posts, I had described my journey of how I compiled \`MariaDB\` on MacOS. It was a struggle and a lot of back and forth but in the process, I learnt about some dependencies that aren't really necessary to compile and run \`MariaDB\`. In this 
+blog post, I will be talking about \`.frm\` (form) files and a parser that I am writing as part of this Google summer of Code.
+
+Google Summer of Code is in motion, with this being a pen-ultimate week before mid evaluations, I am currently contributing in this
+project (https://summerofcode.withgoogle.com/programs/2025/projects/vAVydntH).
+
+In Community bonding period, members of MariaDB community had setup a call where we introduced ourselves and got to know about our mentors. Post that I connected with my mentors separately and they told me how I can approach this problem. I had researched at my end about the \`.frm\` files and where they are used in the \`MariaDB\`.
+
+Through some jira comments mentioned on the raised issue(https://jira.mariadb.org/browse/MDEV-4637). In the comments, Sergei Golubchik describes how we can simply replace the data structures with \`printf\` and we can have a simple parser. I had thought of the similar approach. After my conversation with my mentors, Nikita Malyavin and Oleksandr Sanja Byelkin, we made this approach concrete, My first task was to create a separate utility which simply compile the \`init_from_binary_frm_image\` and works. 
+
+I was given the KT on the process and different data structures by Nikita Malyavin and Oleksandr "Sanja" Byelkin in the discussion call, there I learnt about different \`handlers\` that are being used and the data structures like \`THD\`, \`TABLE\`, \`TABLE_SHARE\`.
+
+In my time, I searched around the format of form files and different tools that are already there in the wild, one particular blog
+that I came across and found helpful was this: https://dbsake.readthedocs.io/en/latest/appendix/frm_format.html#frm-format
+But it was better to trust the implementation in the \`init_from_binary_frm_image\` function which is defined in \`table.cc\` 
+
+\`TABLE_SHARE::init_from_binary_frm_image\`
+
+
+
+Currently when MariaDB starts we have 
+
+---- 
+
+## References
+
+- [MariaDB official Docs](https://mariadb.org/get-involved/getting-started-for-developers/get-code-build-test/)
+- [ChatGPT](https://chatgpt.com/)
+- [CFI directives](https://sourceware.org/binutils/docs/as/CFI-directives.html)
+- [Configuration option](https://mariadb.com/kb/en/configuring-mariadb-with-option-files/)
+- [Understanding MySQL internals](https://www.amazon.in/Understanding-MySQL-Internals-Sasha-Pachev/dp/0596009577)
 `,c=`---
 title: Building and Running MariaDB on Macbook
 date: 2025-02-02
@@ -321,7 +360,7 @@ SELECT * FROM test;
 - [CFI directives](https://sourceware.org/binutils/docs/as/CFI-directives.html)
 - [Configuration option](https://mariadb.com/kb/en/configuring-mariadb-with-option-files/)
 - [Understanding MySQL internals](https://www.amazon.in/Understanding-MySQL-Internals-Sasha-Pachev/dp/0596009577)
-`,d=`---
+`,h=`---
 title: mishap with git rebase, reflog to rescue
 date: 2024-12-10
 description: lesson on Git and what not to do 
@@ -368,7 +407,7 @@ It will show you history of the branches and places it had originated from and h
 ## References
 
 - [Git remote branches](https://git-scm.com/book/en/v2/Git-Branching-Remote-Branches)
-`,h=`---
+`,m=`---
 title: TIL - Git worktree & WCGW even after Server processes request sucessfully
 date: 2025-03-28
 description: Learning more on internals of Git & Web Server 
@@ -499,8 +538,97 @@ Moving forward, I plan to:
 GSoC has been an incredible learning experience. I'm grateful for the opportunity to work with the eunomia-bpf team and contribute to such an innovative project.
 
 Feel free to check out my contributions on [GitHub](https://github.com/eunomia-bpf/bpftime) and reach out if you have any questions about my GSoC experience!
-`,m=`---
-title: Fixing a MacOS while installing Clickhouse 
+`,g=`---
+title: My GSoC'25 Journey with MariaDB 
+date: 2025-09-01
+description: Sharing my experience and learnings from Google Summer of Code 2025 with MariaDB
+visible: true
+tags: GSoC, blog, database
+---
+
+# Adding a standalone FRM Parser in MariaDB
+
+
+## Prologue
+
+MariaDB, is a very popular Relational Database Management System(DBMS). It was forked from MySQL and is currently the 14th Most used database according
+to the [DB-Engines](https://db-engines.com/en/ranking). It also participates in Google Summer of Code(https://www.gsocorganizations.dev/organization/mariadb/) and has been doing so since 2016. 
+
+I have always wanted to contribute to a database and recently I have previously got a PR merged in [ClickHouse](https://github.com/ClickHouse/ClickHouse/pull/80228). I also have one open PR in [MariaDB](https://github.com/MariaDB/server/pull/3839).
+
+Before discovering the organization that I wanted to contribute for GSoC, I looked around many databases and projects for my potential organizations
+where I could invest time. I reached out to folks in MariaDB Zulip chat, I really liked the reception from folks in the organization.
+
+First challenge was to run \`MariaDB\` locally on my system (Macbook Air M2), I faced some issues and I documented that in a blog [post](https://hp77-creator.github.io/blogs/building-understanding-mariaDB). I shared the same in the Zulip chat and there was some really good discussion on it. [Link to the thread](https://mariadb.zulipchat.com/#narrow/channel/118760-New-Members/topic/Blog.20on.20how.20to.20compile.20MariaDB.20on.20MacOS/with/497845761)
+
+I really had a good impression of MariaDB after this incident and I wanted to start working on some issues for this org as soon as possible. I referred to a talk by one of the 
+core contributors to MariaDB on how I can start and it helped me a lot in the journey. [Link to the talk](https://youtu.be/KJtTSVH_aXA?si=nQuyLsTIVkPc1LEI)
+
+I picked up on one of the issues that I discovered on MariaDB's board and raised a PR for the same Then when the projects were announced I focussed on something related to processes and the project of creating a FRM utility parser caught my eye.
+
+I looked on internet on the \`FRM\` file format and what it is. [This documentation](https://dbsake.readthedocs.io/en/latest/appendix/frm_format.html) which I guess was prepared after going through the code in \`MySQL\` or \`MariaDB\`. Anyway, only after I did my own comprehension of logic of \`.frm\` files and how they were
+being used in \`MariaDB\` did I discover that \`frm\` is actually pronounced as 'form' and these 'form' files are basically what \`server\` creates whenever you do the following from a client:
+
+\`\`\`sql
+MariaDB[debugdb] > CREATE TABLE(id INT, name VARCHAR(10));
+\`\`\`
+
+\`server\` will create a \`.frm\` file in the data directory of your server. That's cool and everything but there are instances that some user may want to
+recover some DDL commands which they were not able to backup properly. Currently, a tool called \`mysqlfrm\` exists but it has limitations, those are highlighted in [this](https://jira.mariadb.org/browse/MDEV-18827). I will note down those here as part of problem statement.
+
+## Problem statement
+
+Develop a FRM file parser which does not need to connect to a server and is able to print DDL statement for the Database Administrators. Currently existing tools work well but they will be difficult to maintain as many tools like [dbsake](https://github.com/abg/dbsake) and [go-frm-parser](https://github.com/zing22845/go-frm-parser) parse from the specification that they took out from code, there are some engines which create \`frm\` file bit differently(\`ARCHIVE\` engine is one such example).
+So, It is really important to create a tool which is using similar methods that are being used by \`MariaDB\` server.
+
+
+## Initial approach
+
+I reached out to the org with an approach that we can create one common library something like \`libfrm\` which will be then used by both \`server\` and the new utility tool.
+I had gone through jira comments mentioned on the raised issue(https://jira.mariadb.org/browse/MDEV-4637). In the comments, Sergei Golubchik describes how we can simply replace the data structures with \`printf\` and we can have a simple parser. I had thought of the similar approach. After my conversation with my mentors, Nikita Malyavin and Oleksandr Byelkin, we made this approach concrete, My first task was to create a separate utility which simply compiles the \`init_from_binary_frm_image\` and works. 
+
+## Coding phase
+
+Adding the \`init_from_binary_frm_image\` was a very simple task, I had directly linked it with \`libsql\` and other libraries that are used to build the server.
+Trying to make it work was the real challenge, In initial days of my attempts, I used to compile the whole server and then run my tool, I had named it \`frm-parser\`. 
+Real challenge was resolving all the different errors that are now coming because linker is now getting duplicates from my linked libraries, headers that 
+I am using and the variables that I have defined to make my tool work.
+
+\`THD\` (Thread Descriptor) is a class for every thread that is created
+
+
+## Epilogue
+
+Like how famously Kobe Bryant said:
+> Job's not finished. Job Finished? I don't think so.
+
+Similarily the work here is not done, it's just the start, there are so many more features that can be incorporated, the initial idea of creating a separate
+library can be flirted with. Adding a \`--sql\` option which outputs the DDL command in a \`.sql\` file. Fixing the error handling and so much more.
+I feel the more people use it, the better it will get. That is why I am tagging it the \`v0.1\`. It needs a lot of iterations to get to \`v1.0\`.
+
+
+## Acknowledgements
+I am really grateful to Monty Widenius and Anna Widenius for giving folks like us this wonderful opportunity of contributing to a database like MariaDB.
+I would like to thank Sergei Golubchik for his insights during the proposal phase and giving me the opportunity to work on this. I am really thankful to my mentors Nikita Malyavin and Oleksandr Byelkin for their constant guidance and support, I learnt a lot from our interactions and I hope to continue this. 
+
+
+
+## TL;DR
+
+\`mariadb-frm\` is a standalone utility that is created as part of this GSoC, It has following features:
+- Parse FRM files
+- Parse FRM files created in Engines like ARCHIVE, MEMORY, MyISAM, INNODB, CSV, ARIA
+- Parse FRM files with default expressions like \`default(val)\`
+- Parse FRM files that contain mathematical expressions like \`1 * 2 * 3\` 
+- Parse FRM files containing sequences (exception are \`frm\` files of DDL expressions like \`CREATE SEQUENCE ...\`) 
+- Parse FRM files for some plugin functions like JSON (exception: INET and complex geometric functions are not yet supported) 
+- Provides a \`--debug\` option to stack trace in case of failures
+- uses C++20 features
+
+PR [Link](https://github.com/MariaDB/server/pull/4094)
+
+`,p=`---
+title: Fixing a MacOS issue while installing Clickhouse 
 date: 2025-05-13
 description: how a OS library update can break your DB 
 visible: false
@@ -538,7 +666,7 @@ It was interesting to know that how much an OS can change the stability of a sys
 
 Postgres had it declared when they were creating their software and now their changes were breaking. It was a nice reminder that unless you own everything in the stack, you can't be sure when and where your software can break.
 
-`,g=`---
+`,b=`---
 title: KubeCon + CloudNativeCon 2024, New Delhi 
 date: 2024-12-10
 description: Learnings from the first KubeCon in India 
@@ -547,7 +675,7 @@ visible: false
 
 ## Introduction
 
-Kubecon is a conference conducted by Cloud Nat
+Kubecon is a conference conducted by Cloud Native Foundation
 
 ## What to expect
 
@@ -578,7 +706,7 @@ This blog is built with:
 - Hosted on GitHub Pages
 
 Feel free to check out the source code on my GitHub!
-`,b=`---
+`,f=`---
 title: Outing near a lakehouse 
 date: 2025-05-13
 description: what I learned about Datawarehouses, Delta-lakes and Lakehouses 
@@ -628,7 +756,7 @@ switch your DB, Another issue with DWH is that they are not good in storing unst
 always an ETL(Extract-Transform-Load) step before data is added into these DBs.
 
 Delta Lakes provide one step better clarity and support unstructured or semi-structured data but they also lack the modularity 
-`,p=`---
+`,w=`---
 title: mishap with git rebase, reflog to rescue
 date: 2024-12-10
 description: lesson on Git and what not to do 
@@ -675,7 +803,7 @@ It will show you history of the branches and places it had originated from and h
 ## References
 
 - [Git remote branches](https://git-scm.com/book/en/v2/Git-Branching-Remote-Branches)
-`,f=`---
+`,y=`---
 title: Welcome to my blog
 date: 2024-01-20
 description: Introduction to my technical blog and what to expect
@@ -716,7 +844,7 @@ This blog is built with:
 - Hosted on GitHub Pages
 
 Feel free to check out the source code on my GitHub!
-`;async function k(){var n;const i=[],e=Object.assign({"/src/content/blogs/about-mudlet.md":l,"/src/content/blogs/building-understanding-mariaDB.md":c,"/src/content/blogs/git-rebase.md":d,"/src/content/blogs/git-worktree-debug.md":h,"/src/content/blogs/gsoc24.md":u,"/src/content/blogs/installing-clickhouse-fixing-a-macos-issue.md":m,"/src/content/blogs/kubecon-cloudnativecon.md":g,"/src/content/blogs/outing-near-a-lakehouse.md":b,"/src/content/blogs/support-jetstream-proton.md":p,"/src/content/blogs/welcome.md":f});for(const t in e)try{const o=e[t];if(!o||o.trim()===""){console.warn(`Empty blog post file found: ${t}`);continue}const a=(n=t.split("/").pop())==null?void 0:n.replace(".md","");if(a){const r=w(o,a);r&&i.push(r)}}catch(o){console.error(`Error processing blog post ${t}:`,o);continue}return i.sort((t,o)=>new Date(o.date).getTime()-new Date(t.date).getTime())}async function v(){return(await k()).filter(e=>e.visible!==!1)}async function C(i){try{const e=Object.assign({"/src/content/blogs/about-mudlet.md":l,"/src/content/blogs/building-understanding-mariaDB.md":c,"/src/content/blogs/git-rebase.md":d,"/src/content/blogs/git-worktree-debug.md":h,"/src/content/blogs/gsoc24.md":u,"/src/content/blogs/installing-clickhouse-fixing-a-macos-issue.md":m,"/src/content/blogs/kubecon-cloudnativecon.md":g,"/src/content/blogs/outing-near-a-lakehouse.md":b,"/src/content/blogs/support-jetstream-proton.md":p,"/src/content/blogs/welcome.md":f}),n=`/src/content/blogs/${i}.md`;if(n in e){const t=e[n];return!t||t.trim()===""?(console.warn(`Empty blog post file found: ${n}`),null):w(t,i)}return null}catch(e){return console.error(`Error loading blog post ${i}:`,e),null}}function w(i,e){try{const n=i.split(`---
-`);if(n.length<3)return console.warn(`Invalid frontmatter format in post: ${e}`),null;const t=n[1];if(!t)return console.warn(`Missing frontmatter in post: ${e}`),null;const o=_(t),a=o.title,r=o.date;if(!a||!r)return console.warn(`Missing required frontmatter fields in post: ${e}`),null;const s=o.visible,y=s?s.toLowerCase()==="true":!0,I=o.tags;return{slug:e,title:a,date:r,description:o.description||"",content:n.slice(2).join(`---
-`),visible:y,tags:I}}catch(n){return console.error(`Error parsing blog post ${e}:`,n),null}}function _(i){const e={};try{i.split(`
-`).forEach(n=>{const[t,...o]=n.split(":");if(t&&o.length){const a=t.trim(),r=o.join(":").trim();a&&r&&(a==="tags"?e[a]=r.split(",").map(s=>s.trim()):e[a]=r)}})}catch(n){console.error("Error parsing frontmatter:",n)}return e}export{C as a,v as g};
+`;async function v(){var n;const a=[],e=Object.assign({"/src/content/blogs/about-mudlet.md":l,"/src/content/blogs/adding-frm-parser-part-1.md":d,"/src/content/blogs/building-understanding-mariaDB.md":c,"/src/content/blogs/git-rebase.md":h,"/src/content/blogs/git-worktree-debug.md":m,"/src/content/blogs/gsoc24.md":u,"/src/content/blogs/gsoc25.md":g,"/src/content/blogs/installing-clickhouse-fixing-a-macos-issue.md":p,"/src/content/blogs/kubecon-cloudnativecon.md":b,"/src/content/blogs/outing-near-a-lakehouse.md":f,"/src/content/blogs/support-jetstream-proton.md":w,"/src/content/blogs/welcome.md":y});for(const t in e)try{const o=e[t];if(!o||o.trim()===""){console.warn(`Empty blog post file found: ${t}`);continue}const i=(n=t.split("/").pop())==null?void 0:n.replace(".md","");if(i){const r=I(o,i);r&&a.push(r)}}catch(o){console.error(`Error processing blog post ${t}:`,o);continue}return a.sort((t,o)=>new Date(o.date).getTime()-new Date(t.date).getTime())}async function C(){return(await v()).filter(e=>e.visible!==!1)}async function M(a){try{const e=Object.assign({"/src/content/blogs/about-mudlet.md":l,"/src/content/blogs/adding-frm-parser-part-1.md":d,"/src/content/blogs/building-understanding-mariaDB.md":c,"/src/content/blogs/git-rebase.md":h,"/src/content/blogs/git-worktree-debug.md":m,"/src/content/blogs/gsoc24.md":u,"/src/content/blogs/gsoc25.md":g,"/src/content/blogs/installing-clickhouse-fixing-a-macos-issue.md":p,"/src/content/blogs/kubecon-cloudnativecon.md":b,"/src/content/blogs/outing-near-a-lakehouse.md":f,"/src/content/blogs/support-jetstream-proton.md":w,"/src/content/blogs/welcome.md":y}),n=`/src/content/blogs/${a}.md`;if(n in e){const t=e[n];return!t||t.trim()===""?(console.warn(`Empty blog post file found: ${n}`),null):I(t,a)}return null}catch(e){return console.error(`Error loading blog post ${a}:`,e),null}}function I(a,e){try{const n=a.split(`---
+`);if(n.length<3)return console.warn(`Invalid frontmatter format in post: ${e}`),null;const t=n[1];if(!t)return console.warn(`Missing frontmatter in post: ${e}`),null;const o=D(t),i=o.title,r=o.date;if(!i||!r)return console.warn(`Missing required frontmatter fields in post: ${e}`),null;const s=o.visible,k=s?s.toLowerCase()==="true":!0,_=o.tags;return{slug:e,title:i,date:r,description:o.description||"",content:n.slice(2).join(`---
+`),visible:k,tags:_}}catch(n){return console.error(`Error parsing blog post ${e}:`,n),null}}function D(a){const e={};try{a.split(`
+`).forEach(n=>{const[t,...o]=n.split(":");if(t&&o.length){const i=t.trim(),r=o.join(":").trim();i&&r&&(i==="tags"?e[i]=r.split(",").map(s=>s.trim()):e[i]=r)}})}catch(n){console.error("Error parsing frontmatter:",n)}return e}export{M as a,C as g};
